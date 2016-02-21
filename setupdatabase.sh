aaf46e3708df9980f8f1758ac9eb5522918f28c2
liam.j.duffy@gmail.com <liam.j.duffy@gmail.com>
2016-02-21 19:54:54 +0000

    fixed copy error

--- setupdatabase.sh
+++ setupdatabase.sh
@@ -3,9 +3,6 @@ rm -f db.sqlite3
 rm -r main/migrations
 pip install -r requirements.txt
 python manage.py syncdb --noinput
-
-chown www-data:www-data .
-
 python manage.py makemigrations
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
