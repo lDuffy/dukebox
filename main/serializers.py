9e39521b786db7d9667c58c0d0145b4cd373e626
liam.j.duffy@gmail.com <liam.j.duffy@gmail.com>
2016-02-22 00:09:29 +0000

    added like count.

--- main/serializers.py
+++ main/serializers.py
@@ -1,6 +1,5 @@
 from models import Event, Song, CmsUser, Like
 from rest_framework import serializers
-from rest_framework.authtoken.models import Token
 
 BASE_READONLY_FIELDS = [
     'order',
@@ -11,30 +10,17 @@ BASE_READONLY_FIELDS = [
 
 
 class UserSerializer(serializers.ModelSerializer):
-    def create(self, validated_data, *args, **kwargs):
-        user = CmsUser.objects.create(
-            email=validated_data['email'],
-            password=validated_data['password'],
-            first_name=validated_data['first_name'],
-            last_name=validated_data['last_name'],
-            username=validated_data['username']
-        )
-
-        user.save()
-        user.token = Token.objects.create(user=user)
-        user.save()
-
-        return user
-
     class Meta:
         model = CmsUser
         exclude = ['user_permissions', 'groups']
 
 
 class SongSerializer(serializers.HyperlinkedModelSerializer):
+    likes = serializers.IntegerField(source='like_set.count', read_only=True)
+
     class Meta:
         model = Song
-        exclude = ["modified", "created", "uid", "url"]
+        fields = ('title', 'event', 'cmsUser', 'likes')
 
 
 class EventSerializer(serializers.HyperlinkedModelSerializer):
