cd20d2407a889af3025997e1d9012b60906f20f0
liam.j.duffy@gmail.com <liam.j.duffy@gmail.com>
2016-04-13 20:50:28 +0100

    added boolean field. Made migrations

--- main/models.py
+++ main/models.py
@@ -71,6 +71,7 @@ class Event(BaseModel):
     place = models.CharField(max_length=300, null=True, blank=True)
     start_date = models.DateTimeField(default=datetime.now, blank=True)
     end_date = models.DateTimeField(default=datetime.now, blank=True)
+    public = models.BooleanField(default=True)
 
 
 class Song(BaseModel):
