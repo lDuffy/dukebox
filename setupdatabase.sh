d5c46538557dd2830549daa8d7fda63483aef5d0
liam.j.duffy@gmail.com <liam.j.duffy@gmail.com>
2016-04-25 21:04:15 +0100

    updated setupdatabase

--- setupdatabase.sh
+++ setupdatabase.sh
@@ -3,7 +3,6 @@
 #rm -r main/migrations
 pip install -r requirements.txt
 python manage.py syncdb --noinput
-python manage.py makemigrations
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
 python manage.py runscript --traceback load_data
