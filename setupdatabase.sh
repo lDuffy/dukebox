5d1d5958a0055b6244e5d31857a92f9afe55dd23
liam.j.duffy@gmail.com <liam.j.duffy@gmail.com>
2016-05-11 21:47:08 +0100

    updated requirements

--- setupdatabase.sh
+++ setupdatabase.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 pip install -r requirements.txt
-psql -U postgres -h localhost -c "CREATE USER dukebox WITH PASSWORD 'password';"
-psql -U postgres -h localhost -c "ALTER USER dukebox CREATEDB;"
-psql -U postgres -h localhost -c "ALTER ROLE dukebox SUPERUSER;"
-dropdb -U postgres -h localhost dukeboxdb
-createdb -U postgres -h localhost -O dukebox dukeboxdb
-psql -U postgres -h localhost -d dukeboxdb -c "CREATE EXTENSION postgis;"
-psql -U postgres -h localhost -d dukeboxdb -c "CREATE EXTENSION postgis_topology;"
+#psql -U postgres -h localhost -c "CREATE USER dukebox WITH PASSWORD 'password';"
+#psql -U postgres -h localhost -c "ALTER USER dukebox CREATEDB;"
+#psql -U postgres -h localhost -c "ALTER ROLE dukebox SUPERUSER;"
+#dropdb -U postgres -h localhost dukeboxdb
+#createdb -U postgres -h localhost -O dukebox dukeboxdb
+#psql -U postgres -h localhost -d dukeboxdb -c "CREATE EXTENSION postgis;"
+#psql -U postgres -h localhost -d dukeboxdb -c "CREATE EXTENSION postgis_topology;"
 python manage.py makemigrations main
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
