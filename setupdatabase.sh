7246cf3f88c7e641fd807b426105dfeb8e4c0b77
lDuffy <u!oaRsT1>
2016-09-10 13:14:25 +0100

    made some changes

--- setupdatabase.sh
+++ setupdatabase.sh
@@ -4,8 +4,8 @@ pip install -r requirements.txt
 #psql -U postgres -h localhost -c "CREATE USER dukebox WITH PASSWORD 'password';"
 #psql -U postgres -h localhost -c "ALTER USER dukebox CREATEDB;"
 #psql -U postgres -h localhost -c "ALTER ROLE dukebox SUPERUSER;"
-#dropdb -U postgres -h localhost dukeboxdb
-#createdb -U postgres -h localhost -O dukebox dukeboxdb
+dropdb -U postgres -h localhost dukeboxdb
+createdb -U postgres -h localhost -O dukebox dukeboxdb
 #psql -U postgres -h localhost -d dukeboxdb -c "CREATE EXTENSION postgis;"
 #psql -U postgres -h localhost -d dukeboxdb -c "CREATE EXTENSION postgis_topology;"
 python manage.py makemigrations main
